5dc6422b73f7a4c28af451ac11f2a75a2c5cf26e
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 20:29:00 +0530

    [Prabhu] Add: DayOfWeek

---
+++ LogicalPrograms/DayOfWeek.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms;
+
+public class DayOfWeek
+{
+    public static void DayofWeek()
+    {
+        Console.WriteLine("Welcome to the Gregorian Calendar");
+        Console.WriteLine("For month \nSelect Type 1:January, Type 2:Feburary, Type 3:March, Type 4:April," +
+            " Type 5:May, Type 6:June, Type 7:July, Type 8:August, Type 9:September, " +
+            "Type 10:October, Type 11:November, Type 12:December,");
+        Console.WriteLine("Enter the month ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the date of the month");
+        int d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the year");
+        int y = Convert.ToInt32(Console.ReadLine());
+        int y0 = y - (14 - num) / 12;
+        int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+        int m0 = num + 12 * ((14 - num) / 12) - 2;
+        int d0 = (d + x + 31 * m0 / 12) % 7;
+        switch (d0)
+        {
+            case 0:
+                Console.WriteLine("Sunday");
+                break;
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,13 +8,16 @@ public class Program
         bool flag = true;
         while (flag)
         {
-            Console.WriteLine("\nEnter your choice \n1.Coupon Number \n2.Exit");
+            Console.WriteLine("\nEnter your choice \n1.Coupon Number \n2.Day of Week \n3.Exit");
             Console.WriteLine("\nEnter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     CouponNumber.GenerateCoupons(10);
+                    break;
+                case 2:
+                    DayOfWeek.DayofWeek();
                     break;
                 default:
                     flag = false;
