1d91dd2419a7585f888fa1a017e58782c15cc924
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 20:59:44 +0530

    [Prabhu] Add: TemperatureConversion

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -9,7 +9,7 @@ public class Program
         while (flag)
         {
             Console.WriteLine("\n\nEnter your choice \n1.Coupon Number \n2.Day of Week \n3.Fibonacci Series \n4.Perfect Number" +
-                " \n5.Prime Number \n6.Reverse Number \n7.Stop Watch \n8.Exit");
+                " \n5.Prime Number \n6.Reverse Number \n7.Stop Watch \n8.Temperature Conversion \n9.Exit");
             Console.WriteLine("Enter option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -47,6 +47,10 @@ public class Program
                 case 7:
                     StopWatch stopWatch = new StopWatch();
                     stopWatch.Start();
+                    break;
+                case 8:
+                    TemperatureConversion temperatureConversion = new TemperatureConversion();
+                    temperatureConversion.Conversion();
                     break;
                 default:
                     flag = false;
---
+++ LogicalPrograms/TemperatureConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms;
+
+public class TemperatureConversion
+{
+    public void Conversion()
+    {
+        double F, C;
+        Console.WriteLine("Enter the value of Celsius:");
+        C = Convert.ToDouble(Console.ReadLine());
+        F = C * 9 / 5 + 32;
+        Console.WriteLine(C + "°C in Fahrenheit is: " + F + "°F");
+        Console.WriteLine("Enter the value of Fahrenheit:");
+        F = Convert.ToDouble(Console.ReadLine());
+        C = (F - 32) * 5 / 9;
+        Console.WriteLine(F + "°F in Celsius is: " + C + "°C");
+    }
+}
