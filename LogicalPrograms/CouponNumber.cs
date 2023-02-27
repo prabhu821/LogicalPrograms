c9d0e4c2fee6d076fc0cc64774c7b890b2b28bb1
rishiraj007 <rishiraj@mounttalent.com>
2023-02-27 20:25:35 +0530

    [Prabhu] Add: Coupon Number

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms;
+
+public class CouponNumber
+{
+    public static int count = 0;
+    public static int GenerateRandom()
+    {
+        Random random = new Random();
+        int randomNum = random.Next(0, 11);
+        return randomNum;
+    }
+    public static void GenerateCoupons(int num)
+    {
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int count = 0;
+            int random = GenerateRandom();
+            for (int j = 0; j < num; j++)
+            {
+                if (arr[j] == random)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                arr[i] = random;
+            }
+            foreach (var data in arr)
+            {
+                if (data != 0)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,5 +5,21 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Logical Programs");
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\nEnter your choice \n1.Coupon Number \n2.Exit");
+            Console.WriteLine("\nEnter option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CouponNumber.GenerateCoupons(10);
+                    break;
+                default:
+                    flag = false;
+                    break;
+            }
+        }
     }
 }
